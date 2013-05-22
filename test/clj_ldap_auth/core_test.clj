6244e91f4b75ed39024b6bf6f7490f14a7d17ea7
Michael Rowe <mrowe@mojain.com>
2013-05-22 16:37:52 +1000

    Remove placholder test.

--- test/clj_ldap_auth/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clj-ldap-auth.core-test
-  (:require [clojure.test :refer :all]
-            [clj-ldap-auth.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
