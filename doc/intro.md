7adf5a879b6560f4ba88cf53b554fe9a40421317
Michael Rowe <mrowe@mojain.com>
2013-05-22 13:03:07 +1000

    Clean up things we don't need

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to clj-ldap-auth
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- src/clj_ldap_auth/core.clj
+++
@@ -1,9 +0,0 @@
-(ns clj-ldap-auth.core
-  (:gen-class))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  ;; work around dangerous default behaviour in Clojure
-  (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
