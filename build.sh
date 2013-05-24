f2cd6c1b23f9cb9936f9ac4291fa5b574702ecb5
Michael Rowe <mrowe@mojain.com>
2013-05-24 11:41:55 +1000

    A build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+function install_lein {
+    echo "Installing lein..."
+    mkdir -p $(dirname ${LEIN})
+    curl -o ${LEIN} https://raw.github.com/technomancy/leiningen/preview/bin/lein
+    chmod +x ${LEIN}
+}
+
+function lein {
+    export LEIN="${HOME}/bin/lein"
+    [ -x ${LEIN} ] || install_lein
+    ${LEIN} do $@
+}
+
+lein clean, check, jar, doc
